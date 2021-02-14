f47fe0c8b4821c8ab7b866473dd5cf6fdb47e8d3
Moustafa Mahmoud <moustafafci25@gmail.com>
2021-02-14 10:55:03 +0200

    Feat : Update server and app files

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
-
+const port = 8000;
 
 // Setup Server
+
+// Spin up the server
+const server = app.listen(port, listening);
+// Callback to debug
+function listening() {
+    console.log(`App Running on localhost:${port}`);
+}
+
+// Initialize all route with a callback function
+app.get('/all', sendData);
+// Callback function to complete GET '/all'
+function sendData(req, res) {
+    res.send(projectData);
+}
+// Post Route
+app.post('/addWeather', addData);
+
+function addData(req, res) {
+    projectData.temp = req.body.temp;
+    projectData.date = req.body.date;
+    projectData.feelings = req.body.feelings;
+    res.end();
+    console.log(projectData);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,68 @@
 /* Global Variables */
-
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const key = '&APPID=6dcd2b83fb29b9252343b21537c8acee';
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', action);
+
+function action() {
+    const zipCode = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    console.log(newDate);
+    getTemp(baseURL, zipCode, key).then(data => {
+        postData('http://localhost:8000/addWeather', { temp: data.main.temp, date: newDate, feelings: feelings }).then(() => {
+            // update user interface
+            updateUI();
+        });
+    });
+}
+
+// function get temperature
+const getTemp = async (baseURL, code, key) => {
+    const res = await fetch(baseURL + code + key);
+    console.log(res);
+    try {
+        const data = await res.json();
+        console.log(data);
+        return data;
+    } catch (e) {
+        // statements
+        console.log('Error ' + e);
+    }
+}
+
+// post data
+const postData = async (url = '', data = {}) => {
+    const req = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    try {
+        const newData = await req.json();
+        console.log(newData);
+        return newData;
+    } catch (e) {
+        console.log('Error ' + e);
+    }
+}
+
+// update user interface
+const updateUI = async () => {
+    const request = await fetch('http://localhost:8000/all');
+    try {
+        const data = await request.json();
+        console.log('updateUI');
+        document.getElementById('date').innerHTML = data.date;
+        document.getElementById('temp').innerHTML = data.temp;
+        document.getElementById('content').textContent = data.feelings;
+    } catch (e) {
+        console.log(e);
+    }
+}
